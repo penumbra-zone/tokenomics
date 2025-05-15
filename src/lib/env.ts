623639a8d8c3f6b6222a82467fbe59e29d81d671
Alexis Wolfsdorf <alexiswolfsdorf@gmail.com>
2025-05-15 11:32:18 -0300

    feat: db connection

---
+++ src/lib/env.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+interface EnvVars {
+  DATABASE_URL: string
+}
+
+function getEnvVar(key: keyof EnvVars): string {
+  const value = process.env[key]
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not set`)
+  }
+  return value
+}
+
+export const env: EnvVars = {
+  DATABASE_URL: getEnvVar('DATABASE_URL'),
+} 
