b8954d311657a3794bb6e1908e153d1df88d32af
Alexis Wolfsdorf <alexiswolfsdorf@gmail.com>
2025-05-26 20:00:49 -0300

    feat: remove theming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.svg" sizes="any" />
       </head>
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,15 +2,8 @@
 
 import { Provider } from "react-redux";
 
-import { ThemeProvider } from "@/components/theme-provider";
 import { store } from "@/store/store";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <Provider store={store}>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-        {children}
-      </ThemeProvider>
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 }
