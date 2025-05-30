23759c87e37db9e17f708a5b89145c537b1407b8
Alexis Wolfsdorf <alexiswolfsdorf@gmail.com>
2025-05-30 19:26:33 -0300

    fix: mock data

--- src/lib/db/pindexer/mock/mock-pindexer-connection.ts
+++ src/lib/db/pindexer/mock/mock-pindexer-connection.ts
@@ -277,19 +277,11 @@ export class MockPindexerConnection extends AbstractPindexerConnection {
         category: "Staked",
         percentage: 40,
         amount: 400000000,
-        subcategories: [
-          { name: "Validators", amount: 300000000, percentage: 75 },
-          { name: "Delegators", amount: 100000000, percentage: 25 },
-        ],
       },
       {
         category: "DEX Liquidity",
         percentage: 25,
         amount: 250000000,
-        subcategories: [
-          { name: "PEN/USDC", amount: 150000000, percentage: 60 },
-          { name: "PEN/ETH", amount: 100000000, percentage: 40 },
-        ],
       },
       { category: "Community Pool", percentage: 15, amount: 150000000 },
       { category: "Circulating", percentage: 20, amount: 200000000 },
